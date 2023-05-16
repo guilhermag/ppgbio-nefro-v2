2fa6667af040c162b4349f1554a7872bdcaa237c
Guilherme Gabriel <guilhermag@gmail.com>
2023-05-16 08:27:25 -0300

    adding references

--- src/components/steps/0-initial-step/InitialStep.tsx
+++ src/components/steps/0-initial-step/InitialStep.tsx
@@ -1,35 +1,63 @@
-// Step 1
 import './InitialStep.css';
 import { CheckerNextStep } from '../../../shared/interfaces/form';
 import { useEffect } from 'react';
 import initialImage from '../../../shared/assets/initial-img.jpeg';
+import { Link } from '@mui/material';
 
 export const InitialStep = ({ selectNextStep }: CheckerNextStep) => {
   useEffect(() => {
     selectNextStep(1);
   });
   return (
-    <>
+    <div>
       <h2 className='initial'>
         Calculadora para triagem de paciente com doença renal crônica
       </h2>
-      <div className='step-content'>
-        <p className='subtitle-initial'>
-          NefroCheck, a sua fonte rápida de auxílio para encaminhamento
-          nefrologico.
-        </p>
-      </div>
-      <div className='last-container'>
-        <div className='names'>
-          <ul className='list'>
-            <li>Consultores</li>
-            <li>Karina Litchteneker</li>
-            <li>Guilherme de Araujo Gabriel</li>
-            <li>Jefferson Gustavo Martins</li>
-          </ul>
+      <div className='initial-content'>
+        <div className='references'>
+          <div className='reference-item'>
+            <p>
+              Cadernos de atenção primária: rastreamento, Ministério da Saúde,
+              2013.
+            </p>
+            <Link
+              href='https://bvsms.saude.gov.br/bvs/publicacoes/rastreamento_caderno_atencao_primaria_n29.pdf'
+              target='_blank'
+              sx={{ fontSize: 14 }}
+            >
+              Disponível em: Biblioteca virtual em saúde.
+            </Link>
+          </div>
+
+          <div className='reference-item'>
+            <p>
+              Protocolos de encaminhamento da atenção básica para a atenção
+              especializada, Endocrinologia e Nefrologia, Ministério da Saúde,
+              Universidade Federal do Rio Grande do Sul,2015.
+            </p>
+            <Link
+              href='http://biblioteca.cofen.gov.br/wp-content/uploads/2015/11/protocolos_atencao_basica_atencao_especializada.pdf'
+              target='_blank'
+              sx={{ fontSize: 14 }}
+            >
+              Disponível em: Biblioteca virtual Cofen.
+            </Link>
+          </div>
+
+          <div className='last-container'>
+            <div className='names'>
+              <ul className='list'>
+                <li>Consultores</li>
+                <li>Karina Litchteneker</li>
+                <li>Guilherme de Araujo Gabriel</li>
+                <li>Jefferson Gustavo Martins</li>
+              </ul>
+            </div>
+          </div>
         </div>
+
         <img src={initialImage} alt='initial-image' className='initial-image' />
       </div>
-    </>
+    </div>
   );
 };
