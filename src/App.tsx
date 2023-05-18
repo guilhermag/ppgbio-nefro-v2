d4c4deaa0050bb8e5a29dbc2771a791c91e026e3
Guilherme Gabriel <guilhermag@gmail.com>
2023-05-18 18:18:45 -0300

    adding firebase and updating gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,18 @@ import {
   FinalStep,
 } from './components/steps/';
 import { InitialStep } from './components/steps/0-initial-step/InitialStep';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
 
 function App() {
   const [nextStep, setNextStep] = useState(0);
